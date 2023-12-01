ba0a777a256e79fe7e2a2b68c7440b2fce058f60
TeetatB <teetatmix36108@gmail.com>
2023-12-01 15:17:18 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int n;
+    int e = 0;
+    int o = 0;
+    while(n != 0){
+        cout << "Enter an integer: ";
+        cin >> n;
+        if(n%2 == 0 and n !=0) {
+            e=e+1;
+        }
+        if(n%2 !=0) {
+            o=o+1;
+        }
+    }
+    cout << "#Even numbers = " <<e<<"\n"; 
+    cout << "#Odd numbers = " <<o;
     return 0;
 }
